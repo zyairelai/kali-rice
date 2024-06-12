99d61364491ac0c72a7548eb7ee5b2f56db8e2fb
zyairelai <zyairelai@gmail.com>
2024-06-12 20:48:39 +0800

    Update setup.sh

--- kali-2022-2/setup.sh
+++ kali-2022-2/setup.sh
@@ -6,13 +6,16 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 # Extract Rockyou.txt
-sudo echo "set linenumbers" >> ~/.nanorc
-sudo mkdir -p /var/www/uploads
-sudo wget https://raw.githubusercontent.com/zyairelai/kali-rice/master/dotfiles/upload.php -O /var/www/html/upload.php
-sudo chmod a+x /var/www/html/upload.php
+echo "set linenumbers" >> .nanorc
+echo "[general] \n  colors = true" >> ~/.ffufrc
+echo -e "# Change the prefix key to C-a\nset -g prefix C-a\nunbind C-b\nbind C-a send-prefix\nset-option -g mouse on" > .tmux.conf
 sudo chown -R kali /opt
 sudo chown -R kali /var/www
 sudo gzip -d /usr/share/wordlists/rockyou.txt.gz
+
+# sudo mkdir -p /var/www/uploads
+# sudo wget https://raw.githubusercontent.com/zyairelai/kali-rice/master/dotfiles/upload.php -O /var/www/html/upload.php
+# sudo chmod a+x /var/www/html/upload.php
 
 # Enable Login without Password
 sudo cp /etc/lightdm/lightdm.conf /etc/lightdm/lightdm.conf.bak
@@ -26,26 +29,21 @@ autologin-user-timeout=0
 # Installing necessarily tools
 # sudo dpkg --add-architecture i386
 sudo apt update
-sudo apt install tilix docker.io docker-compose alacarte build-essential golang feroxbuster seclists neofetch htop rofi remmina kali-grant-root kali-root-login -y
-sudo systemctl start docker
-sudo chmod 666 /var/run/docker.sock
+sudo apt install alacarte build-essential golang feroxbuster seclists neofetch htop rofi remmina kali-grant-root kali-root-login -y
 wget https://download.sublimetext.com/sublime-text_build-3211_amd64.deb
 wget https://code.visualstudio.com/sha/download\?build=stable\&os=linux-deb-x64 -O vscode.deb
 sudo dpkg -i *.deb
+
+# Installing Docker
+# sudo apt install docker.io docker-compose
+# sudo systemctl start docker
+# sudo chmod 666 /var/run/docker.sock
 
 # Python2 pip
 wget https://gist.githubusercontent.com/anir0y/a20246e26dcb2ebf1b44a0e1d989f5d1/raw/a9908e5dd147f0b6eb71ec51f9845fafe7fb8a7f/pip2%2520install -O run.sh 
 chmod +x run.sh # wget will save the output as run.sh
 ./run.sh # execute to install the pip2
 pip2 install virtualenv
-
-# Personal Terminal Shortcuts
-sudo ln -s /usr/bin/clear /usr/bin/c
-sudo ln -s /usr/bin/screen /usr/bin/sc
-echo "#\!/bin/bash\nrm -rf ~/.john" > cjohn
-echo "#\!/bin/bash\nrm ~/.ssh/known_hosts" > cssh
-chmod a+x cssh && sudo mv cssh /usr/bin/
-chmod a+x cjohn && sudo mv cjohn /usr/bin/
 
 # ZSH Setup
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
